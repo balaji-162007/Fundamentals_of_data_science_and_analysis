87f06549e3c58ea443f209c0726b7be361a8d564
balaji-162007 <balaji46641@gmail.com>
2026-02-17 19:23:55 +0530

    Update 4_b_Correlation_and_scatter_plots.py

--- 4_b_Correlation_and_scatter_plots.py
+++ 4_b_Correlation_and_scatter_plots.py
@@ -1,14 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
-mu = 0
-sigma = 1
-data = np.random.normal(mu, sigma, 1000)
-plt.hist(data, bins=30, density=True, alpha=0.6, color='g')
-xmin, xmax = plt.xlim()
-x = np.linspace(xmin, xmax, 100)
-p = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-0.5 * ((x - mu) / sigma)**2)
-plt.plot(x, p, 'k', linewidth=2)
-plt.xlabel('Data values')
-plt.ylabel('Probability density')
-plt.title('Normal Distribution Curve')
+import numpy as np
+x = np.random.rand(100)
+y = 2 * x + np.random.normal(0, 0.1, 100)
+plt.scatter(x, y, alpha=0.7)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Scatter Plot: X vs Y')
 plt.show()
