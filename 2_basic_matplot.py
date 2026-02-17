e218d8a6b5039be7d76b77b9d77565c6db9898dc
balaji-162007 <balaji46641@gmail.com>
2026-02-17 18:58:33 +0530

    Replace old plot with a customized line plot
    Removed old plotting code and added a new customized line plot.

--- 2_basic_matplot.py
+++ 2_basic_matplot.py
@@ -1,44 +1,8 @@
 import matplotlib.pyplot as plt
-
-# Data
-a = [1, 2, 3, 4, 5]
-b = [0, 0.6, 0.2, 15, 10, 8, 16, 21]
-c = [4, 2, 6, 8, 3, 20, 13, 15]
-
-# Plot lines
-plt.plot(a, label='1st Rep')
-plt.plot(b, "or", label='2nd Rep')   # o = circle, r = red
-plt.plot(list(range(0, 22, 3)), label='3rd Rep')
-plt.plot(c, label='4th Rep')
-
-# Naming axes
-plt.xlabel('Day ->')
-plt.ylabel('Temp ->')
-
-# Get current axes
-ax = plt.gca()
-
-# Hide top and right borders
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-
-# Set left boundary limits
-ax.spines['left'].set_bounds(-3, 40)
-
-# Set x-axis ticks
-plt.xticks(list(range(-3, 10)))
-
-# Set y-axis ticks
-plt.yticks(list(range(-3, 20, 3)))
-
-# Legend
-plt.legend()
-
-# Annotation
-plt.annotate('Temperature V / s Days', xy=(1.01, -2.15))
-
-# Title
-plt.title('All Features Discussed')
-
-# Show plot
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+plt.plot(x, y, color='green', linestyle='--', marker='o', markersize=10)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Customized Line Plot')
 plt.show()
